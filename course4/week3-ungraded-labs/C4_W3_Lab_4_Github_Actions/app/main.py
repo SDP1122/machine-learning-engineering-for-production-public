bae190e480d7103cdf1c27a18754bc81fd2c2932
SDP1122 <106468131+SDP1122@users.noreply.github.com>
2023-09-18 14:34:51 +0800

    Update main.py

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
 
-
+#Test comment
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
